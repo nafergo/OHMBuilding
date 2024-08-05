445b60dba88428ed94a38003065a4d5e667eb142
nelson gonçalves <nafergo@gmail.com>
2024-08-05 14:34:04 +0100

    Update apis.js

--- src/apis.js
+++ src/apis.js
@@ -1,19 +1,19 @@
 let apis = {
   bounding: {
-    api:'https://api.openstreetmap.org/api/0.6/map?bbox=',
+    api:'https://api.openhistoricalmap.org/api/0.6/map?bbox=',
     url: (left, bottom, right, top) => {
       return apis.bounding.api + left + ',' + bottom + ',' + right + ',' + top;
     },
   },
   getRelation: {
-    api:'https://api.openstreetmap.org/api/0.6/relation/',
+    api:'https://api.openhistoricalmap.org/api/0.6/relation/',
     parameters:'/full',
     url: (relationId) => {
       return apis.getRelation.api + relationId + apis.getRelation.parameters;
     },
   },
   getWay: {
-    api:'https://api.openstreetmap.org/api/0.6/way/',
+    api:'https://api.openhistoricalmap.org/api/0.6/way/',
     parameters:'/full',
     url: (wayId) => {
       return apis.getWay.api + wayId + apis.getWay.parameters;
